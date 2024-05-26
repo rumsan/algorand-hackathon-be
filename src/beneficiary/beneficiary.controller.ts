f96c7f00219746b8ada78e066e701d7de5b6ee6f
Rumsan-Asim <asim.neupane@rumsan.net>
2024-05-26 17:37:31 +0545

    chore: Remove unused code and update BeneficiaryController API endpoint

--- src/beneficiary/beneficiary.controller.ts
+++ src/beneficiary/beneficiary.controller.ts
@@ -1,13 +1,48 @@
-import { Controller, Get, Body, Res, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Body,
+  Res,
+  Post,
+  Query,
+  DefaultValuePipe,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { BeneficiaryService } from './beneficiary.service';
-import { CreateBeneficiaryDto } from './dto/send-mail.dto'
+import { CreateBeneficiaryDto, GetBeneficiaryDto } from './dto/send-mail.dto';
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('beneficiary')
+@ApiTags('Beneficiary')
 export class BeneficiaryController {
   constructor(private readonly beneficiaryService: BeneficiaryService) {}
 
+  @Get()
+  @ApiOperation({ summary: 'List of all Beneficiary' })
+  @ApiQuery({ name: 'limit', required: false, type: String })
+  @ApiQuery({ name: 'page', required: false, type: String })
+  @ApiQuery({ name: 'name', required: false, type: String })
+  @ApiQuery({ name: 'walletAddress', required: false, type: String })
+  @ApiResponse({
+    status: 200,
+    description: 'The found record',
+  })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  getBlogs(
+    @Query('limit', new DefaultValuePipe(4), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+    @Query() getBeneficiaryDto: GetBeneficiaryDto,
+  ) {
+    console.log('getBeneficiaryDto', getBeneficiaryDto);
+    // const { email, walletAddress } = getBeneficiaryDto;
+    // const search = { email, walletAddress };
+
+    // return this.beneficiaryService.findAll(limit, page, search);
+  }
   @Post('create-ben')
   sendMail(@Body() sendMailDTO: CreateBeneficiaryDto, @Res() response: any) {
+    // console.log('pugyo yeha samma');
+    // console.log('sendMailDTO', sendMailDTO);
     const mail = this.beneficiaryService.sendMail(sendMailDTO);
 
     return response.status(200).json({
@@ -15,11 +50,7 @@ export class BeneficiaryController {
       mail,
     });
   }
- 
-  @Post('add')
-  create(@Body() sendMailDTO: CreateBeneficiaryDto){
-    
-  }
 
-  
+  @Post('add')
+  create(@Body() sendMailDTO: CreateBeneficiaryDto) {}
 }
--- src/beneficiary/beneficiary.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BeneficiaryService } from './beneficiary.service';
-import { BeneficiaryController } from './beneficiary.controller';
-
-@Module({
-  controllers: [BeneficiaryController],
-  providers: [BeneficiaryService],
-})
-export class BeneficiaryModule {}
