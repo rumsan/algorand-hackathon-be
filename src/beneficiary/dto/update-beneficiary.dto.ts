633e9e5ecd5af79696df87233c68a48db14e3042
Rumsan-Asim <asim.neupane@rumsan.net>
2024-05-26 17:37:31 +0545

    chore: Remove unused code and update BeneficiaryController API endpoint

--- src/beneficiary/dto/update-beneficiary.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateBeneficiaryDto } from './send-mail.dto';
-
-export class UpdateBeneficiaryDto extends PartialType(CreateBeneficiaryDto) {}
