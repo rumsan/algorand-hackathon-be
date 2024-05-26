26ccb2710f5908970f161dc7e5bd2817c45966e8
Rumsan-Asim <asim.neupane@rumsan.net>
2024-05-26 17:35:34 +0545

    chore: Add Prisma schema and Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { CustomExceptionFilter } from './utils/exceptions/exception.filter';
-import { PrismaClientExceptionFilter } from './utils/exceptions/prisma-exception';
+import { VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
+
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api').enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
   app.enableCors();
   app.useGlobalFilters(new CustomExceptionFilter());
   app.useGlobalPipes(
@@ -15,7 +22,17 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  console.log("Starting on port: 5500")
-  await app.listen(5500);
+
+  const config = new DocumentBuilder()
+    .setTitle('Rahat DAPP')
+    .setDescription('The Api description')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+  const PORT = 5500;
+
+  await app.listen(PORT);
+  logger.log(`🚀 Application is running on: http://localhost:${PORT}`);
 }
 bootstrap();
