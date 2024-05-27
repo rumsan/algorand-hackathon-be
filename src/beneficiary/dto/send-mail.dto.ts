a40b73472e259727f4614c805ba0242c1fd8e9a3
Rumsan-Asim <asim.neupane@rumsan.net>
2024-05-27 11:44:31 +0545

    chore: Refactor BeneficiaryService to improve code readability and maintainability

--- src/beneficiary/dto/send-mail.dto.ts
+++ src/beneficiary/dto/send-mail.dto.ts
@@ -1,32 +1,85 @@
-import {IsNotEmpty, IsNumber, IsOptional, IsString} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateBeneficiaryDto {
+  @ApiProperty({
+    description: 'Email of the beneficiary',
+    example: 'beneficiary@gmail.com',
+  })
+  @IsNotEmpty()
+  @IsString()
+  email: string;
 
-    @IsNotEmpty()
-    @IsString()
-    email: string
+  @ApiProperty({
+    description: 'Name of the beneficiary',
+    example: 'John Doe',
+  })
+  @IsNotEmpty()
+  @IsString()
+  name: string;
 
-    @IsNotEmpty()
-    @IsString()
-    name: string
+  @ApiProperty({
+    description: 'Age of the beneficiary',
+    example: 25,
+  })
+  @IsNotEmpty()
+  @IsNumber()
+  age: number;
 
-    @IsNotEmpty()
-    @IsNumber()
-    age: number
+  @ApiProperty({
+    description: 'gender of the beneficiary',
+    example: 'male',
+  })
+  @IsNotEmpty()
+  @IsString()
+  gender: string;
 
-    @IsNotEmpty()
-    @IsString()
-    gender: string
+  @ApiProperty({
+    description: 'Wallete Address of the beneficiary',
+    example: 'skdjfkowje9023ednkjwef',
+  })
+  @IsNotEmpty()
+  @IsString()
+  walletAddress: string;
 
-    @IsNotEmpty()
-    @IsString()
-    walletAddress: string
-    
-
-    @IsNotEmpty()
-    @IsString()
-    mnemonics: string
-    
+  @ApiProperty({
+    description: 'mnemonics of the beneficiary',
+    example: 'cat dog rat ...',
+  })
+  @IsNotEmpty()
+  @IsString()
+  mnemonics: string;
 }
 
+export class GetBeneficiaryDto {
+  @IsOptional()
+  @IsString()
+  @ApiProperty({
+    description: 'title of the blog',
+    example: 'Nepal the great',
+  })
+  email: string;
 
+  @ApiProperty({
+    description: 'Wallete Address of the beneficiary',
+    example: 'skdjfkowje9023ednkjwef',
+  })
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
+  walletAddress: string;
+
+  @IsOptional()
+  @ApiProperty({
+    description: 'Page',
+    example: '1',
+  })
+  page?: string;
+
+  @IsOptional()
+  @ApiProperty({
+    description: 'Limit',
+    example: '3',
+  })
+  limit?: string;
+}
