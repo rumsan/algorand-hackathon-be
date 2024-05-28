8b403c32bb7754de5a103ccef23138c608744102
sushant-rumsan <sushanttripathee1154@gmail.com>
2024-05-28 19:08:02 +0545

    temp

--- prisma/migrations/20240526082432_/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Voucher" (
-    "uuid" TEXT NOT NULL,
-    "voucherName" TEXT NOT NULL,
-    "voucherSymbol" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Voucher_voucherSymbol_key" ON "Voucher"("voucherSymbol");
